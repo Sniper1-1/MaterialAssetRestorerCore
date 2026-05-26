40f968a36fc142c0b02f074dbf922717465601a5
Justin Hooker <justinhooker1@gmail.com>
2026-05-26 17:46:51 -0400

    removed redundancies

--- MaterialAssetRestorerCore/Patches/LeverPatch.cs
+++ MaterialAssetRestorerCore/Patches/LeverPatch.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 using Dawn.Internal;
-using DunGen;
 using HarmonyLib;
 using UnityEngine;
 
--- MaterialAssetRestorerCore/Patches/Network.cs
+++ MaterialAssetRestorerCore/Patches/Network.cs
@@ -1,5 +1,3 @@
-using System.Linq;
-using HarmonyLib;
 using LethalNetworkAPI;
 
 
